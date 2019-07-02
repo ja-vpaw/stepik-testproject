8e238b602fae30aef9e816ed707dc759e28d5ccf
ja <vpaw6@ya.ru>
2019-07-02 16:18:37 +0300

    add login_page check

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        # Проверка на корректный url адрес
+        assert "/login" in self.browser.current_url, "Current url not contain '/login'"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        # Проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        # Проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
---
+++ test_login_page.py
@@ -0,0 +1,26 @@
+from pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+
+
+def test_guest_should_see_login_url(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_guest_should_see_registration_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+def test_guest_should_see_login_page(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
