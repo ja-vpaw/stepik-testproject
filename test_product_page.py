2d7a68904f1c1fd2ae0c79c5892223c22761a177
ja <vpaw6@ya.ru>
2019-07-03 15:29:07 +0300

    add parametrization in product_page check

--- test_product_page.py
+++ test_product_page.py
@@ -1,42 +1,51 @@
+import pytest
 from pages.product_page import ProductPage
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+links = ['http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear',
+         'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+        ]
 
 
-def test_guest_can_add_product_to_cart(browser):
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_cart(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket_check()
 
 
-def test_should_be_add_to_basket_button(browser):
+@pytest.mark.parametrize('link', links)
+def test_should_be_add_to_basket_button(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_button()
 
 
-def test_should_be_message_success_add_to_basket(browser):
+@pytest.mark.parametrize('link', links)
+def test_should_be_message_success_add_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket_click()
     page.should_be_message_success_add_to_basket()
 
 
-def test_should_be_product_price_add_to_basket_price(browser):
+@pytest.mark.parametrize('link', links)
+def test_should_be_product_price_add_to_basket_price(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_price_add_to_basket_price()
 
 
-def test_get_product_price(browser):
+@pytest.mark.parametrize('link', links)
+def test_get_product_price(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket_click()
     page.get_product_price()
 
 
-def test_get_current_basket_value(browser):
+@pytest.mark.parametrize('link', links)
+def test_get_current_basket_value(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket_click()
